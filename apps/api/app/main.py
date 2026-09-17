d9195bd7a570092a14c478a20347d6b7eb0463c9
Alla Naga Lokesh Sai <lokesh8286235@gmail.com>
2026-09-16 21:28:02 -0500

    security: restrict API CORS methods and headers

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -23,8 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=_cors_origins(),
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type"],
 )
 
 
